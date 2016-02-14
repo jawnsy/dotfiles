4d3fbb3eaecaf0ee6b41418805c4755568bcc112
Jonathan Yu <jawnsy@cpan.org>
2016-02-14 12:03:13 -0500

    Add shell terminal options
    Signed-off-by: Jonathan Yu <jawnsy@cpan.org>

---
+++ src/.bashrc.d/terminal.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# If not running interactively, don't do anything
+[ -z "$PS1" ] && return
+
+# check the window size after each command and, if necessary,
+# update the values of LINES and COLUMNS.
+shopt -s checkwinsize
+
+# Don't use ^D to exit
+set -o ignoreeof

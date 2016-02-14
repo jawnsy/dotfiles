42498a9d34238a466e6e1716fbd4d4ef373c9244
Jonathan Yu <jawnsy@cpan.org>
2016-02-14 12:03:26 -0500

    Bash history settings
    Signed-off-by: Jonathan Yu <jawnsy@cpan.org>

---
+++ src/.bashrc.d/history.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# If not running interactively, don't do anything
+[ -z "$PS1" ] && return
+
+# don't put duplicate lines or lines starting with space in the history.
+# See bash(1) for more options
+HISTCONTROL=ignoreboth
+
+# append to the history file, don't overwrite it
+shopt -s histappend
+
+# for setting history length see HISTSIZE and HISTFILESIZE in bash(1)
+HISTSIZE=1000
+HISTFILESIZE=2000
+
+# Clear bash's command hash just in case.
+hash -r

aa8f790269d5659a93c02210c05f955758d46a64
Jonathan Yu <jawnsy@cpan.org>
2016-12-26 18:15:07 -0800

    Use named colors for prompt
    Signed-off-by: Jonathan Yu <jawnsy@cpan.org>

--- src/.bashrc.d/prompt.bash
+++ src/.bashrc.d/prompt.bash
@@ -3,17 +3,27 @@
 # If not running interactively, don't do anything
 [ -z "$PS1" ] && return
 
-PS1="\[\e[1;37m\]\A "
-if [ $EUID -eq 0 ]; then
-  PS1="$PS1\[\e[1;31m\]\h \[\e[0;32m\]\w \[\e[1;33m\]# \[\e[0m\]"
+reset='\[\e[m\]'
+normal_green='\[\e[0;32m\]'
+normal_yellow='\[\e[0;33m\]'
+normal_purple='\[\e[0;35m\]'
+normal_cyan='\[\e[0;36m\]'
+bright_gray='\[\e[1;30m\]'
+bright_red='\[\e[1;31m\]'
+bright_yellow='\[\e[1;33m\]'
+bright_white='\[\e[1;37m\]'
+
+PS1="${bright_white}\A "
+if [ ${EUID} -eq 0 ]; then
+  PS1+="${bright_red}\h ${normal_green}\w ${bright_yellow}# ${reset}"
 else
-  PS1="$PS1\[\e[0;35m\]\u\[\e[1;30m\]@\[\e[0;32m\]\h\[\e[1;30m\]:\[\e[1;33m\]\w\[\e[1;30m\]\[\e[1;37m\] $ \[\e[0m\]"
+  PS1+="${normal_purple}\u${bright_gray}@${normal_green}\h ${normal_yellow}\w ${reset}\$ "
 fi
 export PS1
+export PS2="${bright_white}> ${reset}"
+export PS3="${bright_white}? ${reset}"
+export PS4="${bright_white}+ ${reset}"
 
-export PS2="\[\e[1;37m\]> \[\e[0m\]"
-export PS3="\[\e[1;37m\]? \[\e[0m\]"
-export PS4="\[\e[1;37m\]+ \[\e[0m\]"
 
 export PROMPT_COMMAND='echo -ne "\033]0;${USER}@$(hostname -s): ${PWD}\007"'
 

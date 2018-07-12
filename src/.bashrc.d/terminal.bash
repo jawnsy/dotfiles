e0434606f37f575455847dc82b93b11265d95ea4
Jonathan Yu <jawnsy@cpan.org>
2018-07-12 07:41:39 -0700

    Update shell options
    * Enable cdspell: automatically fix minor typos in "cd" directory
      names
    * Enable globstar: include subdirectories with "**" wildcard
    * Disable sourcepath: do not search PATH when sourcing scripts
    * Enable notify: notify when background jobs terminate
    * Enable pipefail: return exit status of failed command in pipeline
    
    Signed-off-by: Jonathan Yu <jawnsy@cpan.org>

--- src/.bashrc.d/terminal.bash
+++ src/.bashrc.d/terminal.bash
@@ -1,9 +1,23 @@
 # If not running interactively, don't do anything
 [ -z "$PS1" ] && return
 
-# check the window size after each command and, if necessary,
-# update the values of LINES and COLUMNS.
+# Check window size after each command and update LINES and COLUMNS
 shopt -s checkwinsize
+
+# Automatically fix minor typos in "cd" directory names
+shopt -s cdspell
+
+# Include subdirectories when expanding "**" pattern
+shopt -s globstar
+
+# Do not search PATH when using "source" builtin
+shopt -u sourcepath
+
+# Notify when background jobs terminate
+set -o notify
+
+# Return exit status of failed command in pipeline
+set -o pipefail
 
 # Don't use ^D to exit
 set -o ignoreeof

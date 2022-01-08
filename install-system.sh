39d6b5a0cf8035a7a1ea69bc429c3baa85cb9c99
Jonathan Yu <jawnsy@cpan.org>
2022-01-08 11:08:56 -0800

    feat: add more package repositories
    * Add Microsoft Edge repository
    * Prevent VSCode from installing repository list
    * Add NodeSource repository
    * Add Yarnpkg repository

--- install-system.sh
+++ install-system.sh
@@ -23,33 +23,72 @@ echo "--> Removing redundant/unnecessary files..."
 
 FILES="$PROJECT_ROOT/files"
 
-if prompt "Install Chrome repositories? (y/N)"; then
-    make_directory "/etc/apt/preferences.d"
-    copy "$FILES/etc/apt/preferences.d/google-chrome" "/etc/apt/preferences.d/google-chrome"
+make_directory "/etc/apt/preferences.d"
+make_directory "/etc/apt/sources.list.d"
+make_directory "/etc/default"
+make_directory "/usr/local/share/keyrings"
 
-    make_directory "/etc/apt/sources.list.d"
+if prompt "Install Google Chrome repositories? (y/N)"; then
+    copy "$FILES/etc/apt/preferences.d/google-chrome" "/etc/apt/preferences.d/google-chrome"
     copy "$FILES/etc/apt/sources.list.d/google-chrome.list" "/etc/apt/sources.list.d/google-chrome.list"
 
-    make_directory "/etc/default"
-    copy "$FILES/etc/default/google-chrome" "/etc/default/google-chrome"
-
-    make_directory "/usr/local/share/keyrings"
+    # Install repository-specific keyring
     copy "$FILES/usr/local/share/keyrings/google-chrome.gpg" "/usr/local/share/keyrings/google-chrome.gpg"
+
+    # Prevent Chrome from adding repositories and keys, since
+    # we want to manage these ourselves.
+    copy "$FILES/etc/default/google-chrome" "/etc/default/google-chrome"
 else
     remove "/etc/apt/sources.list.d/google-chrome.list"
 fi
 
-if prompt "Install Microsoft Visual Studio Code repositories? (y/N)"; then
-    make_directory "/etc/apt/preferences.d"
-    copy "$FILES/etc/apt/preferences.d/microsoft-vscode" "/etc/apt/preferences.d/microsoft-vscode"
+if prompt "Install Microsoft Edge repositories? (y/N)"; then
+    copy "$FILES/etc/apt/preferences.d/microsoft-edge" "/etc/apt/preferences.d/microsoft-edge"
+    copy "$FILES/etc/apt/sources.list.d/microsoft-edge.list" "/etc/apt/sources.list.d/microsoft-edge.list"
 
-    make_directory "/etc/apt/sources.list.d"
+    # Install repository-specific keyring
+    copy "$FILES/usr/local/share/keyrings/microsoft.gpg" "/usr/local/share/keyrings/microsoft.gpg"
+
+    # Prevent Edge from adding repositories and keys, since
+    # we want to manage these ourselves.
+    copy "$FILES/etc/default/microsoft-edge-beta" "/etc/default/microsoft-edge-beta"
+    copy "$FILES/etc/default/microsoft-edge-dev" "/etc/default/microsoft-edge-dev"
+else
+    remove "/etc/apt/sources.list.d/microsoft-edge.list"
+fi
+
+if prompt "Install Microsoft Visual Studio Code repositories? (y/N)"; then
+    copy "$FILES/etc/apt/preferences.d/microsoft-vscode" "/etc/apt/preferences.d/microsoft-vscode"
     copy "$FILES/etc/apt/sources.list.d/microsoft-vscode.list" "/etc/apt/sources.list.d/microsoft-vscode.list"
 
-    make_directory "/usr/local/share/keyrings"
+    # Install repository-specific keyring
     copy "$FILES/usr/local/share/keyrings/microsoft.gpg" "/usr/local/share/keyrings/microsoft.gpg"
+
+    # VSCode will look for a vscode.list file, but we want to manage
+    # things manually, so ensure it's empty.
+    echo >"/etc/apt/sources.list.d/vscode.list"
 else
     remove "/etc/apt/sources.list.d/microsoft-vscode.list"
+fi
+
+if prompt "Install NodeSource repositories? (y/N)"; then
+    copy "$FILES/etc/apt/preferences.d/nodesource" "/etc/apt/preferences.d/nodesource"
+    copy "$FILES/etc/apt/sources.list.d/nodesource.list" "/etc/apt/sources.list.d/nodesource.list"
+
+    # Install repository-specific keyring
+    copy "$FILES/usr/local/share/keyrings/nodesource.gpg" "/usr/local/share/keyrings/nodesource.gpg"
+else
+    remove "/etc/apt/sources.list.d/nodesource.list"
+fi
+
+if prompt "Install Yarnpkg repositories? (y/N)"; then
+    copy "$FILES/etc/apt/preferences.d/yarnpkg" "/etc/apt/preferences.d/yarnpkg"
+    copy "$FILES/etc/apt/sources.list.d/yarnpkg.list" "/etc/apt/sources.list.d/yarnpkg.list"
+
+    # Install repository-specific keyring
+    copy "$FILES/usr/local/share/keyrings/yarnpkg.gpg" "/usr/local/share/keyrings/yarnpkg.gpg"
+else
+    remove "/etc/apt/sources.list.d/yarnpkg.list"
 fi
 
 echo
